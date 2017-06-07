c2d1209d8c34ba7a04d4ea8e2747d607cdff1da9
Matt Borja <matt.borja@gmail.com>
2017-06-07 11:24:14 -0700

    Fix broken $protocol variable in schannel_protocol.pp

--- manifests/schannel_protocol.pp
+++ manifests/schannel_protocol.pp
@@ -1,24 +1,24 @@
 define windowsciphers::schannel_protocol($enable_protocol = undef, $disable_protocol = undef) {
 	if $disable_protocol != undef {
-		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${protocol}\\Server\\Enabled":
+		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${disable_protocol}\\Server\\Enabled":
 			ensure => present,
 			type => dword,
 			data => 0x00000000
 		}
 
-		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${protocol}\\Server\\DisabledByDefault":
+		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${disable_protocol}\\Server\\DisabledByDefault":
 			ensure => present,
 			type => dword,
 			data => 0x00000001
 		}
 
-		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${protocol}\\Client\\Enabled":
+		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${disable_protocol}\\Client\\Enabled":
 			ensure => present,
 			type => dword,
 			data => 0x00000000
 		}
 
-		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${protocol}\\Client\\DisabledByDefault":
+		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${disable_protocol}\\Client\\DisabledByDefault":
 			ensure => present,
 			type => dword,
 			data => 0x00000001
@@ -26,25 +26,25 @@ define windowsciphers::schannel_protocol($enable_protocol = undef, $disable_prot
 	}
 
 	if $enable_protocol != undef {
-		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${protocol}\\Server\\Enabled":
+		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${enable_protocol}\\Server\\Enabled":
 			ensure => present,
 			type => dword,
 			data => 0xffffffff
 		}
 
-		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${protocol}\\Server\\DisabledByDefault":
+		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${enable_protocol}\\Server\\DisabledByDefault":
 			ensure => present,
 			type => dword,
 			data => 0x00000000
 		}
 
-		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${protocol}\\Client\\Enabled":
+		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${enable_protocol}\\Client\\Enabled":
 			ensure => present,
 			type => dword,
 			data => 0xffffffff
 		}
 
-		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${protocol}\\Client\\DisabledByDefault":
+		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${enable_protocol}\\Client\\DisabledByDefault":
 			ensure => present,
 			type => dword,
 			data => 0x00000000
