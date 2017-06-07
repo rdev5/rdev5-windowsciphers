1ca6b58a388cc8aaba1fcde8a058cf755c980ebb
Matt Borja <matt.borja@gmail.com>
2017-06-07 11:26:28 -0700

    Fix broken algorithm variables in schannel_key_algorithm.pp

--- manifests/schannel_key_algorithm.pp
+++ manifests/schannel_key_algorithm.pp
@@ -1,14 +1,14 @@
-define windowsciphers::schannel_key_algorithm($enable_algorithm = undef, $disable_algorithm = undef) {
-	if $disable_algorithm != undef {
-		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\KeyExchangeAlgorithms\\${disable_algorithm}\\Enabled":
+define windowsciphers::schannel_key_algorithm($enable_key_algorithm = undef, $disable_key_algorithm = undef) {
+	if $disable_key_algorithm != undef {
+		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\KeyExchangeAlgorithms\\${disable_key_algorithm}\\Enabled":
 			ensure => present,
 			type => dword,
 			data => 0x00000000
 		}
 	}
 
-	if $enable_algorithm != undef {
-		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\KeyExchangeAlgorithms\\${enable_algorithm}\\Enabled":
+	if $enable_key_algorithm != undef {
+		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\KeyExchangeAlgorithms\\${enable_key_algorithm}\\Enabled":
 			ensure => present,
 			type => dword,
 			data => 0xffffffff
