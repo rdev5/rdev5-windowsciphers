bf0f70a05207a279348a184c3028c602ca89d2db
Matt Borja <matt.borja@gmail.com>
2017-06-07 09:11:25 -0700

    Add windowsciphers::schannel::enable

--- manifests/schannel.pp
+++ manifests/schannel.pp
@@ -29,6 +29,37 @@ class windowsciphers::schannel::disable ($protocols = [], $ciphers = [], $hashes
 	}
 }
 
+class windowsciphers::schannel::enable ($protocols = [], $ciphers = [], $hashes = [], $keyAlgorithms = []) {
+	validate_array($protocols)
+	validate_array($ciphers)
+	validate_array($hashes)
+	validate_array($keyAlgorithms)
+
+	$protocols.each |$protocol| {
+		windowsciphers::schannel::protocol::enable { $protocol:
+			protocol => $protocol
+		}
+	}
+
+	$ciphers.each |$cipher| {
+		windowsciphers::schannel::cipher::enable { $cipher:
+			cipher => $cipher
+		}
+	}
+
+	$hashes.each |$hash| {
+		windowsciphers::schannel::hash::enable { $hash:
+			hash => $hash
+		}
+	}
+
+	$keyAlgorithms.each |$algorithm| {
+		windowsciphers::schannel::key_exchange::enable { $algorithm:
+			algorithm => $algorithm
+		}
+	}
+}
+
 # Ciphers
 define windowsciphers::schannel::cipher::disable($cipher) {
 	registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Ciphers\\${cipher}\\Enabled":
