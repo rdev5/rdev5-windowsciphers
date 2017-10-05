bd7742548ccac5bc370b302ba213505057a6d8dc
Matt Borja <matt.borja@gmail.com>
2017-10-05 11:25:30 -0700

    Declare registry_key resources for adding missing keys (RE: puppetlabs-registry@MODULES-4446)

--- manifests/schannel_protocol.pp
+++ manifests/schannel_protocol.pp
@@ -1,63 +1,102 @@
+# TODO: Refactor into helpers and cleanup
 define windowsciphers::schannel_protocol($enable_protocol = undef, $disable_protocol = undef) {
 	if $disable_protocol != undef {
-		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${disable_protocol}\\Server\\Enabled":
+		$disable_server_protocol_key = "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${disable_protocol}\\Server"
+		$disable_client_protocol_key = "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${disable_protocol}\\Client"
+		
+		registry_key { $disable_server_protocol_key: ensure => present }
+		registry_key { $disable_client_protocol_key: ensure => present }
+
+		# Disable server protocols
+		registry_value { "${disable_server_protocol_key}\\Enabled":
 			ensure => present,
 			type => dword,
-			data => 0x00000000
+			data => 0x00000000,
+			require => [
+				Registry_key[$disable_server_protocol_key],
+				Registry_key[$disable_client_protocol_key],
+			],
 		}
 
-		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${disable_protocol}\\Server\\DisabledByDefault":
+		registry_value { "${disable_server_protocol_key}\\DisabledByDefault":
 			ensure => present,
 			type => dword,
-			data => 0x00000001
+			data => 0x00000001,
+			require => [
+				Registry_key[$disable_server_protocol_key],
+				Registry_key[$disable_client_protocol_key],
+			],
 		}
 
-		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${disable_protocol}\\Client\\Enabled":
+		# Disable client protocols
+		registry_value { "${disable_client_protocol_key}\\Enabled":
 			ensure => present,
 			type => dword,
-			data => 0x00000000
+			data => 0x00000000,
+			require => [
+				Registry_key[$disable_server_protocol_key],
+				Registry_key[$disable_client_protocol_key],
+			],
 		}
 
-		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${disable_protocol}\\Client\\DisabledByDefault":
+		registry_value { "${disable_client_protocol_key}\\DisabledByDefault":
 			ensure => present,
 			type => dword,
-			data => 0x00000001
+			data => 0x00000001,
+			require => [
+				Registry_key[$disable_server_protocol_key],
+				Registry_key[$disable_client_protocol_key],
+			],
 		}
 	}
 
 	if $enable_protocol != undef {
-		$registry_key = "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${enable_protocol}"
+		$enable_server_protocol_key = "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${enable_protocol}\\Server"
+		$enable_client_protocol_key = "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${enable_protocol}\\Client"
 		
-		registry_key { $registry_key:
-			ensure => present,
-		}
+		registry_key { $enable_server_protocol_key: ensure => present }
+		registry_key { $enable_client_protocol_key: ensure => present }
 
-		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${enable_protocol}\\Server\\Enabled":
+		# Enable server protocols
+		registry_value { "${enable_server_protocol_key}\\Enabled":
 			ensure => present,
 			type => dword,
 			data => 0xffffffff,
-			require => Registry_key[$registry_key],
+			require => [
+				Registry_key[$enable_server_protocol_key],
+				Registry_key[$enable_client_protocol_key],
+			],
 		}
 
-		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${enable_protocol}\\Server\\DisabledByDefault":
+		registry_value { "${enable_server_protocol_key}\\DisabledByDefault":
 			ensure => present,
 			type => dword,
 			data => 0x00000000,
-			require => Registry_key[$registry_key],
+			require => [
+				Registry_key[$enable_server_protocol_key],
+				Registry_key[$enable_client_protocol_key],
+			],
 		}
 
-		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${enable_protocol}\\Client\\Enabled":
+		# Enable client protocols
+		registry_value { "${enable_client_protocol_key}\\Enabled":
 			ensure => present,
 			type => dword,
 			data => 0xffffffff,
-			require => Registry_key[$registry_key],
+			require => [
+				Registry_key[$enable_server_protocol_key],
+				Registry_key[$enable_client_protocol_key],
+			],
 		}
 
-		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${enable_protocol}\\Client\\DisabledByDefault":
+		registry_value { "${enable_client_protocol_key}\\DisabledByDefault":
 			ensure => present,
 			type => dword,
 			data => 0x00000000,
-			require => Registry_key[$registry_key],
+			require => [
+				Registry_key[$enable_server_protocol_key],
+				Registry_key[$enable_client_protocol_key],
+			],
 		}
 	}
 }
