759910754786c6b74da745d17eee9d93f824cdbc
Matt Borja <matt.borja@gmail.com>
2017-10-05 11:11:21 -0700

    Add registry_key to ensure key exists in schannel_protocol (enable)

--- manifests/schannel_protocol.pp
+++ manifests/schannel_protocol.pp
@@ -26,28 +26,38 @@ define windowsciphers::schannel_protocol($enable_protocol = undef, $disable_prot
 	}
 
 	if $enable_protocol != undef {
+		$registry_key = "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${enable_protocol}"
+		
+		registry_key { $registry_key:
+			ensure => present,
+		}
+
 		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${enable_protocol}\\Server\\Enabled":
 			ensure => present,
 			type => dword,
-			data => 0xffffffff
+			data => 0xffffffff,
+			require => Registry_key[$registry_key],
 		}
 
 		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${enable_protocol}\\Server\\DisabledByDefault":
 			ensure => present,
 			type => dword,
-			data => 0x00000000
+			data => 0x00000000,
+			require => Registry_key[$registry_key],
 		}
 
 		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${enable_protocol}\\Client\\Enabled":
 			ensure => present,
 			type => dword,
-			data => 0xffffffff
+			data => 0xffffffff,
+			require => Registry_key[$registry_key],
 		}
 
 		registry_value { "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${enable_protocol}\\Client\\DisabledByDefault":
 			ensure => present,
 			type => dword,
-			data => 0x00000000
+			data => 0x00000000,
+			require => Registry_key[$registry_key],
 		}
 	}
 }
