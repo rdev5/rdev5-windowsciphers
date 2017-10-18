d657b1d40fa29513022412704adeb3121ed63f0d
Matt Borja <matt.borja@gmail.com>
2017-10-18 15:05:38 -0700

    Separate client protocols in SCHANNEL with $copyToClient (default: false)

--- manifests/schannel_protocol.pp
+++ manifests/schannel_protocol.pp
@@ -1,20 +1,17 @@
 # TODO: Refactor into separate functions
-define windowsciphers::schannel_protocol($enable_protocol = undef, $disable_protocol = undef) {
+define windowsciphers::schannel_protocol($enable_protocol = undef, $disable_protocol = undef, $copyToClient = false) {
 	if $disable_protocol != undef {
+		# Disable server protocols
 		$disable_server_protocol_key = "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${disable_protocol}\\Server"
-		$disable_client_protocol_key = "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${disable_protocol}\\Client"
 		
 		registry_key { $disable_server_protocol_key: ensure => present }
-		registry_key { $disable_client_protocol_key: ensure => present }
 
-		# Disable server protocols
 		registry_value { "${disable_server_protocol_key}\\Enabled":
 			ensure => present,
 			type => dword,
 			data => 0x00000000,
 			require => [
 				Registry_key[$disable_server_protocol_key],
-				Registry_key[$disable_client_protocol_key],
 			],
 		}
 
@@ -24,47 +21,47 @@ define windowsciphers::schannel_protocol($enable_protocol = undef, $disable_prot
 			data => 0x00000001,
 			require => [
 				Registry_key[$disable_server_protocol_key],
-				Registry_key[$disable_client_protocol_key],
 			],
 		}
 
 		# Disable client protocols
-		registry_value { "${disable_client_protocol_key}\\Enabled":
-			ensure => present,
-			type => dword,
-			data => 0x00000000,
-			require => [
-				Registry_key[$disable_server_protocol_key],
-				Registry_key[$disable_client_protocol_key],
-			],
-		}
+		if $copyToClient {
+			$disable_client_protocol_key = "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${disable_protocol}\\Client"
+			
+			registry_key { $disable_client_protocol_key: ensure => present }
 
-		registry_value { "${disable_client_protocol_key}\\DisabledByDefault":
-			ensure => present,
-			type => dword,
-			data => 0x00000001,
-			require => [
-				Registry_key[$disable_server_protocol_key],
-				Registry_key[$disable_client_protocol_key],
-			],
+			registry_value { "${disable_client_protocol_key}\\Enabled":
+				ensure => present,
+				type => dword,
+				data => 0x00000000,
+				require => [
+					Registry_key[$disable_client_protocol_key],
+				],
+			}
+
+			registry_value { "${disable_client_protocol_key}\\DisabledByDefault":
+				ensure => present,
+				type => dword,
+				data => 0x00000001,
+				require => [
+					Registry_key[$disable_client_protocol_key],
+				],
+			}
 		}
 	}
 
 	if $enable_protocol != undef {
+		# Enable server protocols
 		$enable_server_protocol_key = "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${enable_protocol}\\Server"
-		$enable_client_protocol_key = "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${enable_protocol}\\Client"
 		
 		registry_key { $enable_server_protocol_key: ensure => present }
-		registry_key { $enable_client_protocol_key: ensure => present }
 
-		# Enable server protocols
 		registry_value { "${enable_server_protocol_key}\\Enabled":
 			ensure => present,
 			type => dword,
 			data => 0xffffffff,
 			require => [
 				Registry_key[$enable_server_protocol_key],
-				Registry_key[$enable_client_protocol_key],
 			],
 		}
 
@@ -74,29 +71,33 @@ define windowsciphers::schannel_protocol($enable_protocol = undef, $disable_prot
 			data => 0x00000000,
 			require => [
 				Registry_key[$enable_server_protocol_key],
-				Registry_key[$enable_client_protocol_key],
 			],
 		}
 
 		# Enable client protocols
-		registry_value { "${enable_client_protocol_key}\\Enabled":
-			ensure => present,
-			type => dword,
-			data => 0xffffffff,
-			require => [
-				Registry_key[$enable_server_protocol_key],
-				Registry_key[$enable_client_protocol_key],
-			],
-		}
+		if $copyToClient {
+			$enable_client_protocol_key = "HKLM\\System\\CurrentControlSet\\Control\\SecurityProviders\\SCHANNEL\\Protocols\\${enable_protocol}\\Client"
+			
+			registry_key { $enable_client_protocol_key: ensure => present }
 
-		registry_value { "${enable_client_protocol_key}\\DisabledByDefault":
-			ensure => present,
-			type => dword,
-			data => 0x00000000,
-			require => [
-				Registry_key[$enable_server_protocol_key],
-				Registry_key[$enable_client_protocol_key],
-			],
+			# Enable client protocols
+			registry_value { "${enable_client_protocol_key}\\Enabled":
+				ensure => present,
+				type => dword,
+				data => 0xffffffff,
+				require => [
+					Registry_key[$enable_client_protocol_key],
+				],
+			}
+
+			registry_value { "${enable_client_protocol_key}\\DisabledByDefault":
+				ensure => present,
+				type => dword,
+				data => 0x00000000,
+				require => [
+					Registry_key[$enable_client_protocol_key],
+				],
+			}
 		}
 	}
 }
